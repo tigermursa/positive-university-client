fc0805eb97340c7d0eaa555eaf531addfd717e36
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-12 07:54:05 +0600

    "creating new routes path for the sidebar of my dashboard"

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -4,7 +4,31 @@ import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 
 export const adminPaths2 = [
-    
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "/admin/create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "/admin/create-admin",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "/admin/create-admin",
+        element: <CreateStudent />,
+      },
+    ],
+  },
 ];
 
 export const adminPaths = [
