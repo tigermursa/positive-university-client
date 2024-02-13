1ba9f82cd7e1c62124d1ef3094051b6b44f26682
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-13 20:24:10 +0600

    "working on protected routes"

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
 //type for initial state
 type TAuthState = {
     user: null | object;
@@ -32,3 +33,5 @@ const authSlice = createSlice({
 
 export const { setUser, logout } = authSlice.actions;
 export default authSlice.reducer;
+export const useCurrentToken = ((state: RootState) => state.auth.token)
+export const useCurrentUser = ((state: RootState) => state.auth.user)
