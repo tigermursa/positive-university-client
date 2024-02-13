db3ec4fc68031a65230d6d25378a1fe1eeda01e8
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-13 23:18:33 +0600

    "updated login form"

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,7 +23,7 @@ const Login = () => {
     console.log(user);
   };
   return (
-    <div>
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh"}}>
       <form onSubmit={handleSubmit(onSubmit)} style={{border:"2px solid red"}}>
         <div>
           <label htmlFor="id">ID:</label>
